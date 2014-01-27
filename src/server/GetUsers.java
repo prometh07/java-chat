f415c6f85227f265413da33c1db9b3f7e04368fb
Radosław Luter <radekpl2@gmail.com>
2014-01-28 00:50:41 +0100

    GetUsers command implemented.

--- src/server/GetUsers.java
+++ src/server/GetUsers.java
@@ -3,6 +3,9 @@
  */
 package server;
 
+import java.util.*;
+import java.util.concurrent.ConcurrentMap;
+
 /**
  * @author Radosław Luter (radekpl2@gmail.com)
  *
@@ -16,8 +19,28 @@ class GetUsers implements ServerCommand {
 
 	@Override
 	public void executeCommand(Client client, String message) {
-		// TODO Auto-generated method stub
+		ChatServer server = client.getServer();
+		ConcurrentMap<String, Client> connectedClients = server.getConnectedClients();
+		String users = "@USERS:" + getUsers(connectedClients);
+		client.send(users);
+	}
+
+	private String getUsers(ConcurrentMap<String, Client> connectedClients) {
+		List<String> listOfLogins = new ArrayList<String>();
 		
+		for (String clientLogin: connectedClients.keySet()) {
+			listOfLogins.add(clientLogin);
+		}
+		Collections.sort(listOfLogins);
+		
+		StringBuilder builder = new StringBuilder();
+		for(String clientLogin: listOfLogins) {
+			builder.append(clientLogin);
+			builder.append(",");
+		}
+		builder.setLength(builder.length() -1);
+		String users = builder.toString();
+		return users;
 	}
 
 }
