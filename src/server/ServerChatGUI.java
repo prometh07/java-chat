601ac66442a904023b3b3b90f7c8d2d41db35a95
Radosław Luter <radekpl2@gmail.com>
2014-02-16 23:40:23 +0100

    Server GUI added.

---
+++ src/server/ServerChatGUI.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package server;
+
+/**
+ * @author Radosław Luter (radekpl2@gmail.com)
+ *
+ */
+public class ServerChatGUI {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
